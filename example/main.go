d88dc56e2bd6dc0c04665d597f8917b3a735ef93
Matt Olenik <olenikm@gmail.com>
2022-07-20 09:12:59 -0700

    Test printing a command from the tree only if it's been visited by the parser

--- example/main.go
+++ example/main.go
@@ -17,13 +17,17 @@ func main() {
 
 func mainE() error {
 	c := charm.NewCommand("dbdrawer", "dbd something something")
-	_ = c.SubCommand("sub1", "subcommand 1")
+	sc := c.SubCommand("sub1", "subcommand 1")
 
 	_ = charm.FlagF(c, 5, "intval", "the int value")
 	err := c.Parse(os.Args[1:])
 	if err != nil {
 		return err
 	}
-	fmt.Println(c.TreePrint("  "))
+	c.Walk(0, func(d int, c *charm.Command) {
+		if c == sc && c.Visited {
+			fmt.Println(c)
+		}
+	})
 	return nil
 }
